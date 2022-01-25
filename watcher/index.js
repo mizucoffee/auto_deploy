2151fd9c227814263018b73f6eecbfc4dccda1a8
Ritsuki Goto <develop@mizucoffee.net>
2022-01-25 19:24:06 +0900

    [fix] env

--- watcher/index.js
+++ watcher/index.js
@@ -7,7 +7,6 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.listen(42953);
 
-
 exec(`nginx -g "daemon off;"`, (err, stdout, stderr) => {
   if (err) {
     console.error(err);
@@ -19,11 +18,11 @@ exec(`nginx -g "daemon off;"`, (err, stdout, stderr) => {
 
 let ps = null;
 execSync(`git clone "${process.env.GITHUB_REPO}" /website`)
-const branch = process.env.GITHUB_BRANCH || execSync('git symbolic-ref --short HEAD', {cwd: "/website"})
+const branch = process.env.GITHUB_BRANCH || execSync('git symbolic-ref --short HEAD', {cwd: "/website", env: {...process.env}})
 
-if(process.env.GITHUB_BRANCH) execSync(`git checkout ${process.env.GITHUB_BRANCH}`, {cwd: "/website"})
-if(process.env.PRE_COMMAND) execSync(`${process.env.PRE_COMMAND}`, {cwd: "/website"})
-ps = exec(`${process.env.START_COMMAND}`, { env: {PORT: 3000}, detached: true, cwd: "/website" }, (err, stdout, stderr) => {
+if(process.env.GITHUB_BRANCH) execSync(`git checkout ${process.env.GITHUB_BRANCH}`, {cwd: "/website", env: {...process.env}})
+if(process.env.PRE_COMMAND) execSync(`${process.env.PRE_COMMAND}`, {cwd: "/website", env: {...process.env}})
+ps = exec(`${process.env.START_COMMAND}`, { env: {PORT: 3000}, detached: true, cwd: "/website" , env: {...process.env}}, (err, stdout, stderr) => {
   if (err) {
     console.error(err);
     return;
@@ -34,9 +33,9 @@ ps = exec(`${process.env.START_COMMAND}`, { env: {PORT: 3000}, detached: true, c
 
 async function update() {
   process.kill(-ps.pid, 'SIGINT')
-  execSync(`git fetch && git reset --hard origin/$(git symbolic-ref --short HEAD)`, {cwd: "/website"})
-  if(process.env.PRE_COMMAND) execSync(`${process.env.PRE_COMMAND}`, {cwd: "/website"})
-  ps = exec(`${process.env.START_CMD}`, {env: {PORT: 3000}, detached: true, cwd: "/website"}, (err, stdout, stderr) => {
+  execSync(`git fetch && git reset --hard origin/$(git symbolic-ref --short HEAD)`, {cwd: "/website", env: {...process.env}})
+  if(process.env.PRE_COMMAND) execSync(`${process.env.PRE_COMMAND}`, {cwd: "/website", env: {...process.env}})
+  ps = exec(`${process.env.START_CMD}`, {env: {PORT: 3000}, detached: true, cwd: "/website", env: {...process.env}}, (err, stdout, stderr) => {
     if (err) {
       console.error(err);
       return;
